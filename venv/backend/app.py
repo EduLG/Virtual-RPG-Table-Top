217588faabb6a6bbbf3017b442563c2647eecb82
Edu <eduladron@gmail.com>
2025-12-17 19:01:53 +0100

    status: ok

---
+++ venv/backend/app.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+from config import Config
+
+db = SQLAlchemy()
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    CORS(app)
+    db.init_app(app)
+
+    @app.route("/ping")
+    def ping():
+        return {"status": "ok"}
+
+    return app
+
+if __name__ == "__main__":
+    app = create_app()
+    app.run(debug=True)
