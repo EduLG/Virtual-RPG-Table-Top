bedcd6b9e600328f03782907258e99d2aea0d40d
Edu <eduladron@gmail.com>
2025-12-29 13:31:26 +0100

    db config

--- venv/backend/config.py
+++ venv/backend/config.py
@@ -1,5 +1,5 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = "postgresql://usuario:password@localhost:5432/tu_basedatos"
+    SQLALCHEMY_DATABASE_URI = "postgresql://eduladron:12341234@localhost:5432/tu_basedatos"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
