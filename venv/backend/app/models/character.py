0a2e2148236e2dbaf530b36ab0059e534b8bf142
Edu <eduladron@gmail.com>
2025-12-29 22:21:41 +0100

    bugs to be able to execute the backend

--- venv/backend/app/models/character.py
+++ venv/backend/app/models/character.py
@@ -1,12 +1,12 @@
 from ..extensions import db
 
-class Character(db.model):
+class Character(db.Model):
     __tablename__ = 'characters'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.string(50), nullable=False)
+    name = db.Column(db.String(50), nullable=False)
     health = db.Column(db.Integer, default=100)
     power = db.Column(db.Integer, default=10)
     defense = db.Column(db.Integer, default=5)
 
-    user_id = db.Column(db.Integer, db.ForeignKey('user_id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users'), nullable=False)
--- venv/backend/app/models/enemy.py
+++ venv/backend/app/models/enemy.py
@@ -1,10 +1,10 @@
 from ..extensions import db
 
-class Enemy(db.model):
-    __tablename__ = 'Enemies'
+class Enemy(db.Model):
+    __tablename__ = 'enemies'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.string(50), nullable=False)
+    name = db.Column(db.String(50), nullable=False)
     health = db.Column(db.Integer, default=10)
     power = db.Column(db.Integer, default=2)
     defense = db.Column(db.Integer, default=1)
