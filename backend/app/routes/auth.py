62c3b294a743be827085b8701e1f4be3591dec2c
Edu <eduladron@gmail.com>
2026-01-07 18:14:39 +0100

    simple login and register finished

--- backend/app/routes/auth.py
+++ backend/app/routes/auth.py
@@ -1,13 +1,19 @@
 from flask import Blueprint, request, jsonify
 from werkzeug.security import generate_password_hash
+from werkzeug.security import check_password_hash
 from app.extensions import db
 from app.models.user import User
 
 auth_bp = Blueprint("auth", __name__)
 
-@auth_bp.route("/register", method=["post"])
+# ---------------------------------------------------------------REGISTER
+
+@auth_bp.route("/register", methods=["POST"])
 def register():
     data = request.get_json()
+
+    if data is None:
+        return jsonify({"error": "There is no data"}), 400
 
     username = data.get("username")
     email = data.get("email")
@@ -19,17 +25,47 @@ def register():
     existing_user = User.query.filter_by(email=email).first()
 
     if existing_user:
-        return jsonify({"error", "The user already exists"}), 409
-    
-    hasked_password = generate_password_hash(password)
+        return jsonify({"error": "The user already exists"}), 409
+
+    hashed_password = generate_password_hash(password)
 
     new_user = User(
         username=username,
         email=email,
-        password=hasked_password
+        password=hashed_password
     )
 
     db.session.add(new_user)
     db.session.commit()
 
-    return jsonify({"message", "User created succesfully."}), 201
+    return jsonify({"message": "User created succesfully."}), 201
+
+#------------------------------------------------------------------LOGIN
+
+@auth_bp.route("/login", methods=["POST"])
+def login():
+    data = request.get_json()
+
+    if not data:
+        return jsonify({"error": "No data provided"}), 400
+
+    username = data.get("username")
+    password = data.get("password")
+    
+    if not username or not password:
+        return jsonify({"error": "Missing data."}), 400
+    
+    user = User.query.filter_by(username=username).first()
+
+    if not user:
+        return jsonify({"error": "Invalid credentials"}), 401
+    
+    if not check_password_hash(user.password, password):
+        return jsonify({"error": "Invalid credentials"}), 401
+    
+    return jsonify({
+        "message": "Login succesful",
+        "user_id": user.id,
+        "username": user.username
+    }), 200
+    
