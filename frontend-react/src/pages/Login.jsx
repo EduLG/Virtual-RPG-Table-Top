3d58f56857f2b502f2daf8ef31de10a247e13b79
Edu <eduladron@gmail.com>
2026-01-13 10:11:32 +0100

    imports modified for login modal

--- frontend-react/src/pages/Login.jsx
+++ frontend-react/src/pages/Login.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import Modal from "../components/Modal";
+import LoginModal from "../components/modals/LoginModal";
 import { Button } from "primereact/button";
 
 const Login = () => {
