28dc6e0b5c29a9f1e838c45e48a6652d283e54a2
Edu <eduladron@gmail.com>
2026-01-13 13:07:22 +0100

    simple login/register form

--- frontend-react/src/components/modals/LogRegModal.jsx
+++ frontend-react/src/components/modals/LogRegModal.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Button } from "primereact/button";
 import { Dialog } from "primereact/dialog";
+import { InputText } from "primereact/inputtext";
 
 const LoginModal = ({ visible, setVisible, mode }) => {
   return (
@@ -12,7 +13,25 @@ const LoginModal = ({ visible, setVisible, mode }) => {
           setVisible(false);
         }}
       >
-        {mode === "login" ? <p>Login</p> : <p>Register</p>}
+        {mode === "login" ? (
+          <>
+            <h2>Log in</h2>
+            <p>User Name:</p>
+            <InputText />
+            <p>Password:</p>
+            <InputText />
+          </>
+        ) : (
+          <>
+            <h2>Register</h2>
+            <p>Email:</p>
+            <InputText />
+            <p>User Name:</p>
+            <InputText />
+            <p>Password:</p>
+            <InputText />
+          </>
+        )}
       </Dialog>
     </div>
   );
