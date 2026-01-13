a5eba61dab40d87a64c6086968f4e4a8240ffee3
Edu <eduladron@gmail.com>
2026-01-13 10:12:22 +0100

    layour embedded to get the layout

--- frontend-react/src/routes/AppRouter.jsx
+++ frontend-react/src/routes/AppRouter.jsx
@@ -2,12 +2,15 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import Login from "../pages/Login";
 import MainLayout from "../components/MainLayout";
+import LoginLayout from "../components/LoginLayout";
 
 export default function AppRouter() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<Login />} />
+        <Route element={<LoginLayout />}>
+          <Route path="/login" element={<Login />} />
+        </Route>
         <Route element={<MainLayout />}>
           <Route path="/" element={<Home />} />
         </Route>
