0c530a312ea6d56e0425edd286e31572ab8df1c1
Edu <eduladron@gmail.com>
2026-01-14 16:55:19 +0100

    global css added

--- frontend-react/src/main.jsx
+++ frontend-react/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { Theme } from "@radix-ui/themes";
 import "@radix-ui/themes/styles.css";
 import App from "./App.jsx";
+import "./styles/index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
