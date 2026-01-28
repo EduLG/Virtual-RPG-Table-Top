3207e84b33338d733c73725a0f7d072e3b25b3e0
Edu <eduladron@gmail.com>
2026-01-28 18:40:40 +0100

    addings to home layout

--- frontend-react/src/pages/Home.jsx
+++ frontend-react/src/pages/Home.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Button } from "@radix-ui/themes";
 import adventurer from "../assets/resources/character-templates/adventurer.png";
+import bgImage from "../assets/resources/bgImage.png";
 
 const Home = () => {
   return (
-    <div className="flex min-h-screen bg-[#2b1e14]">
+    <div className="flex min-h-screen bg-[#2b1e14] h-screen">
       <aside className="w-1/5 bg-[#3b2a1a] text-[#f5e6c8] p-4 border-r border-[#6b4f2a]">
         <nav className="space-y-4">
           <h2 className="text-xl font-bold">Navbar</h2>
@@ -22,8 +23,11 @@ const Home = () => {
         </nav>
       </aside>
 
-      <main className="w-4/5 p-6 bg-[#2b1e14] h-screen">
-        <div className="grid grid-cols-2 grid-rows-2 gap-6 h-full">
+      <main
+        className="grid grid-cols-[1fr_3fr_1fr] p-6 w-full h-full bg-cover bg-center"
+        style={{ backgroundImage: `url(${bgImage})` }}
+      >
+        <div className="col-start-2 grid grid-cols-2 grid-rows-2 gap-6 h-full">
           {[1, 2, 3, 4].map((card) => (
             <div
               key={card}
