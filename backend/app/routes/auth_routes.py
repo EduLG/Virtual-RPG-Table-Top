7ae6a16ce7a3cfc3fa018e30dd2afe8ee4946b10
Edu <eduladron@gmail.com>
2026-05-23 18:24:05 +0200

    demo auth complete route

--- backend/app/routes/auth_routes.py
+++ backend/app/routes/auth_routes.py
@@ -1,16 +1,20 @@
 from flask import Blueprint
-from app.handlers.auth_handler import register_user_handler, login_user_handler, refresh_token_handler
+from app.handlers.auth_handler import (
+    register_user_handler,
+    login_user_handler,
+    refresh_token_handler,
+    demo_login_handler,
+)
 
 auth_bp = Blueprint("auth", __name__)
 
 # delegate to handler functions (route -> handler -> service -> repository)
 
-#Register
 auth_bp.route("/register", methods=["POST"])(register_user_handler)
 
-#Login
 auth_bp.route("/login", methods=["POST"])(login_user_handler)
 
-#Token refresh
 auth_bp.route("/refresh", methods=["POST"])(refresh_token_handler)
+
+auth_bp.route("/demo", methods=["POST"])(demo_login_handler)
 
